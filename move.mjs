3ed47db8c91dcd2cf6c72f34dac99c873c9aaab1
subframe7536 <1667077010@qq.com>
2022-11-09 22:12:21 +0800

    opt: quote

--- move.mjs
+++ move.mjs
@@ -1,8 +1,21 @@
 import { copyFileSync, existsSync, mkdir } from 'fs'
 import { resolve } from 'path'
-import { dirs } from './dir.js'
-dirs.forEach(dir => {
-  let base = resolve(dir, '.obsidian', 'snippets')
-  !existsSync(base) && mkdir(base)
-  copyFileSync('obsidian.css', resolve(base, 'obsidian.css'))
-})
+/**
+ * in dir.js:
+ *
+ * ```js
+ * export const dirs = ['/path/to/your/vault/root']
+ * ```
+ */
+if (existsSync('./dir.js')) {
+  const { dirs } = await import('./dir.js')
+  console.log(`choose ${dirs.length} vaults`)
+  dirs.forEach(dir => {
+    let vaultRoot = resolve(dir, '.obsidian', 'snippets')
+    !existsSync(vaultRoot) && mkdir(vaultRoot)
+    copyFileSync('obsidian.css', resolve(vaultRoot, 'obsidian.css'))
+    console.log(`apply to ${dir}`)
+  })
+} else {
+  console.log('no assigned vault')
+}
