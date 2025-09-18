a15bda5b1b06ce6755ff323d322fccbd0e598a54
subframe7536 <1667077010@qq.com>
2025-09-18 08:49:11 +0800

    mobile floating button

--- src/style-settings/index.ts
+++ src/style-settings/index.ts
@@ -3,12 +3,14 @@ import editor from './editor'
 import plugin from './plugin'
 import workspace from './workspace'
 import { version } from '../../package.json'
+import mobile from './mobile'
 
 const settings = `/* Maple Theme v${version} For Obsidian */
 ${basic}
 ${workspace}
 ${editor}
 ${plugin}
+${mobile}
 `
 
 if (!process.argv[1].endsWith('compile.ts')) {
---
+++ src/style-settings/mobile.ts
@@ -0,0 +1,14 @@
+import { Settings } from './generator'
+
+export default Settings.create('maple-mobile', 'Maple Mobile').children([
+  Settings.of().addClassToggle(
+    'mobile-floating-button',
+    {
+      title: {
+        en: 'Floating Mode Switch Button',
+        zh: '浮动的模式切换按钮',
+      },
+    },
+    { enable: true },
+  ),
+])
