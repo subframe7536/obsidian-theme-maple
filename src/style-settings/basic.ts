28f10883010db71e5cb22986d9b5acc7eed80e9d
subframe7536 <1667077010@qq.com>
2025-09-26 17:32:52 +0800

    WIP better color scheme

--- src/style-settings/basic.ts
+++ src/style-settings/basic.ts
@@ -19,17 +19,21 @@ export default Settings.create('maple-basic', 'Maple Basic').children([
     },
     { default: 1.5, min: 1, max: 2, step: 0.1 },
   ),
-  Settings.ofLevel(1, { title: { en: 'Theme Color', zh: '主题色' } })
-    .addClassToggle(
+  Settings.ofLevel(1, { title: { en: 'Colors', zh: '颜色' } })
+    .addClassSelect(
       'color-use-custom',
       {
-        title: { en: 'Custom Theme Color', zh: '自定义主题色' },
-        desc: {
-          en: "Use the Style Setting Plugin's theme color instead of Obsidian's",
-          zh: '使用 Style Settings 插件的主题色，而不是 Obsidian 的',
-        },
+        title: { en: 'Color Scheme', zh: '颜色方案' },
       },
-      { enable: false },
+      {
+        allowEmpty: false,
+        default: 'color-use-custom',
+        options: [
+          { label: 'Default', value: 'color-use-default' },
+          { label: 'Mono', value: 'color-use-mono' },
+          { label: 'Custom', value: 'color-use-custom' },
+        ],
+      },
     )
     .children([
       Settings.ofLevel(2, { title: { en: 'Light Mode', zh: '浅色模式' } })
