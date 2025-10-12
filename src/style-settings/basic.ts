a79680ca6920468bceb1c1144b09ec4793a20079
subframe7536 <1667077010@qq.com>
2025-10-12 10:34:51 +0800

    refactor: `--animation-fast` -> `--animation`, `--animation` -> `--animation-slow`

--- src/style-settings/basic.ts
+++ src/style-settings/basic.ts
@@ -6,7 +6,7 @@ export default Settings.create('maple-basic', 'Maple Basic').children([
     {
       title: { en: 'Animation Duration (ms)', zh: '动画持续时间（毫秒）' },
     },
-    { default: 500, min: 0, max: 1000, step: 50, format: 'ms' },
+    { default: 250, min: 0, max: 1000, step: 50, format: 'ms' },
   ),
   Settings.of().addVarNumSlider(
     'setting-item-line-height',
